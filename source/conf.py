66a9d6164f2732ade1214f7516c803e3954f08e8
M. Schulze <mks@e.mail.de>
2022-04-04 18:11:58 +0200

    add logo in sidebar nav + local theme (#11)
    * local template
    
    * fix html_theme_path to incluse custom karma theme
    
    * add sidebar logo
    
    Co-authored-by: Patric Schmitz <bzk0711@aol.com>

--- source/conf.py
+++ source/conf.py
@@ -31,11 +31,11 @@ release = '2021-2022'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-        'myst_parser'
+    'myst_parser',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+# templates_path = ['_templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -48,31 +48,24 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 #html_theme = 'bizstyle'
+html_theme_path = ["."]
 html_theme = 'karma_sphinx_theme'
 html_favicon = 'favicon-32x32.png'
 html_logo = 'a3_logo_dark-200px.png'
 #html_last_updated_fmt = '%b, %d, %Y'
 
-html_theme_options = {
-    'navigation_depth': 2,
-    'includehidden': True,
-    'titles_only': True
-}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = [
+    '_static',
+    'karma_sphinx_theme/static',
+]
 
 source_suffix = {
     '.rst': 'restructuredtext',
 #   '.txt': 'markdown',
     '.md': 'markdown',
 }
-
-
-# Das sollte angeblich css override können
-# https://ofosos.org/2018/12/28/landing-page-template/
-# def setup(app):
-#     app.add_stylesheet('css/custom.css')
 
